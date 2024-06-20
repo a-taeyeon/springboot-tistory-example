daa1fc0d1d3c1c9be891bc6ce7cd71e6f664aece
a-taeyeon <taeyeon0107@naver.com>
2024-06-20 14:55:25 +0900

    feat: Add environment-specific datasource configuration with java-dotenv

---
+++ src/main/java/com/tistory/framework/core/DatasourceConfig.java
@@ -0,0 +1,33 @@
+package com.tistory.framework.core;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Getter
+@Setter
+@Configuration
+@Profile("prod")
+@Slf4j
+public class DatasourceConfig {
+    @Autowired
+    private Dotenv dotenv;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(dotenv.get("PROD_MYSQL_PROJECT_URL"));
+        dataSource.setUsername(dotenv.get("PROD_MYSQL_USERNAME"));
+        dataSource.setPassword(dotenv.get("PROD_MYSQL_PASSWORD"));
+        return dataSource;
+    }
+}
---
+++ src/main/java/com/tistory/framework/core/DotenvConfig.java
@@ -0,0 +1,35 @@
+package com.tistory.framework.core;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod") // 이 클래스는 prod 프로파일에서만 활성화됩니다.
+public class DotenvConfig {
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.configure()
+                .directory("/keys") // resources 디렉토리 내의 경로 (resources 디렉토리는 classpath에 포함됨)
+                .filename("prod.env") // 사용할 환경 변수 파일 명시
+                .ignoreIfMissing() // 파일이 없어도 무시
+                .load();
+    }
+    @Bean
+    public String PROD_MYSQL_PROJECT_URL(Dotenv dotenv) {
+        return dotenv.get("PROD_MYSQL_PROJECT_URL");
+    }
+
+    @Bean
+    public String PROD_MYSQL_USERNAME(Dotenv dotenv) {
+        return dotenv.get("PROD_MYSQL_USERNAME");
+    }
+
+    @Bean
+    public String PROD_MYSQL_PASSWORD(Dotenv dotenv) {
+        return dotenv.get("PROD_MYSQL_PASSWORD");
+    }
+
+
+}
